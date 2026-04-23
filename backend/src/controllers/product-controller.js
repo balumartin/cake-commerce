960e5be8e02c57393d23b0a00662bd45e76dfc0f
balumartin <martinka.balazs@gmail.com>
2026-04-23 14:31:24 +0200

    feat: add query param handling for pagination

--- backend/src/controllers/product-controller.js
+++ backend/src/controllers/product-controller.js
@@ -1,8 +1,13 @@
+import {
+  createProductSchema,
+  updateProductSchema,
+} from "../schemas/product-schema.js";
 import productService from "../services/product-service.js";
 
 const productController = {
-  create: async (req, res, next) => {
+  createProduct: async (req, res, next) => {
     try {
+      await createProductSchema.validate(req.body);
       const { name, description, price, imageUrl } = req.body;
       const product = await productService.createProduct({
         name,
@@ -11,15 +16,48 @@ const productController = {
         imageUrl,
       });
 
-      res.status(201).json(product);
+      res.status(200).json(product);
     } catch (err) {
       next(err);
     }
   },
   getProducts: async (req, res, next) => {
     try {
-      const products = await productService.getProducts();
-      res.status(201).json(products);
+      const { page, limit, search, minPrice, maxPrice } = req.query;
+      const result = await productService.getProducts({
+        page,
+        limit,
+        search,
+        minPrice,
+        maxPrice,
+      });
+      res.status(201).json(result);
+    } catch (err) {
+      next(err);
+    }
+  },
+  updateProduct: async (req, res, next) => {
+    try {
+      await updateProductSchema.validate(req.body);
+      const { name, description, price, imageUrl } = req.body;
+      const product = await productService.updateProduct(req.params.id, {
+        name,
+        description,
+        price,
+        imageUrl,
+      });
+
+      res.json(product);
+    } catch (err) {
+      next(err);
+    }
+  },
+
+  deleteProduct: async (req, res, next) => {
+    try {
+      await productService.deleteProduct(req.params.id);
+
+      res.status(204).send();
     } catch (err) {
       next(err);
     }
