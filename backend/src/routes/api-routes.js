ac12582f7d9b33c4374effc8905f0dad5cb17fb2
balumartin <martinka.balazs@gmail.com>
2026-04-29 09:22:44 +0200

    feat: add category routes

--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./auth-routes.js";
 import userRoutes from "./user-routes.js";
 import productRoutes from "./product-routes.js";
+import categoryRoutes from "./category-routes.js"
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
+router.use("/category", categoryRoutes);
 
 export default router;
